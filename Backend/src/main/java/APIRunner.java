dc2fe2b2882a7d73a91f62937df955b2009bc8f1
keyjpg <93946569+keyjpg@users.noreply.github.com>
2022-12-28 14:16:59 +0100

    template.

--- Backend/src/main/java/APIRunner.java
+++ Backend/src/main/java/APIRunner.java
@@ -1,12 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.javalin.Javalin;
+
 public class APIRunner
 {
 
-    public APIRunner(){
+    private Gson gson = null;
 
+    public APIRunner(){
+        gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:nm:ss").create();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        APIRunner runner = new APIRunner();
+        Javalin app = Javalin.create(/*config*/)
+                .get("/", ctx -> {ctx.html("Hello"); })
+                .start(5000);
     }
 
 }
