a5d79df023e06f06903ce4a253db3bea0241ee53
obed321 <97287335+obed321@users.noreply.github.com>
2022-12-31 23:25:53 +0100

    data in/ut

--- Backend/src/main/java/APIRunner.java
+++ Backend/src/main/java/APIRunner.java
@@ -1,6 +1,22 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+
+;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class APIRunner
 {
@@ -13,11 +29,39 @@ public class APIRunner
 
     public static void main(String[] args){
         APIRunner runner = new APIRunner();
-        Javalin app = Javalin.create(/*config*/)
-                .get("/", ctx -> {ctx.html("MAIN PAGE"); })
-                .get("/asset/{nasa_id}", ctx -> {ctx.html("retrieved planet image"); }) // todo:
-                .get("/id", ctx -> {ctx.html("DESCRIPTION OF IMAGE"); }) // todo:
-                .start(5000);
+        Javalin app = Javalin.create().start(5000);
+        app.get("/", ctx -> {ctx.html("MAIN PAGE"); });
+
+
+
+
+        app.get("metadata/{nasa_id}", ctx -> {
+            String id = ctx.pathParam("nasa_id");
+            String url = "https://images-api.nasa.gov/metadata/" + id;
+            try (InputStream inputStream = new URL(url).openStream()) {
+                String data = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+                ctx.result(data);
+             //   ctx.header("Accept");
+                //ctx.header("Content-Type", "application/json");
+               Map map = new HashMap<String, String>();
+            //    map.put("id", id);
+                map.put("Description ", data + id);
+                ctx.json(map);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
     }
 
-}
+
+
+              //  app.get("/asset/{nasa_id}", ctx -> {ctx.html("retrieved planet image"); }); // todo:
+
+     //   Map map = new HashMap<String, String>();
+     //   map.put("Description", "Infomation " + ctx.pathParam("id"));
+     //   ctx.json(map);
+    }
