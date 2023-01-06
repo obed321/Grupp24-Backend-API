637c4943ce8d9078dd714a24d48759265c6671c9
obed321 <97287335+obed321@users.noreply.github.com>
2023-01-06 12:51:19 +0100

    new apis

--- Backend/src/main/java/APIRunner.java
+++ Backend/src/main/java/APIRunner.java
@@ -1,12 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
-import org.jetbrains.annotations.NotNull;
-import io.javalin.http.Context;
-
-import org.jetbrains.annotations.NotNull;
 
 ;
 import java.io.BufferedReader;
@@ -15,12 +10,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
-import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class APIRunner {
 
@@ -33,41 +26,52 @@ public class APIRunner {
     public static void main(String[] args) {
         APIRunner runner = new APIRunner();
         Javalin app = Javalin.create().start(5000);
-        app.get("/", ctx -> {ctx.html("MAIN PAGE");});
-        app.get("metadata/{nasa_id}", ctx -> {runner.getImageFromNasa(ctx);});
+        app.get("/", ctx -> {
+            ctx.html("MAIN PAGE");
+        });
+        app.get("metadata/{nasa_id}", ctx -> {
+            runner.getImageLocation(ctx);
+        });
 
         //Använd denna api om du vill få picture of the day från nasa
-        app.get("/<version>/apod", ctx -> {runner.getImageAndDescriptionFromNasaApodAPI(ctx);});
+        app.get("/<version>/apod", ctx -> {
+            runner.getImageAndDescriptionFromNasaApodAPI(ctx);
+        });
+        //Dnna api hämtar bild
+        app.get("asset/{nasa_id}", ctx -> {
+            runner.getImageFromNasa(ctx);
+        });
+
+    app.get("/search", ctx -> {runner.searchNasa(ctx);});
     }
 
 
+    //Denna api skickar dagen astronomi bild med text :)
+    public void getImageAndDescriptionFromNasaApodAPI(Context ctx) {
+        String apiKey = "qUiVgPByEvA7mORI5pfuIyhmmgIcJWNIqf6JYdF0";
+        String url = "https://api.nasa.gov/planetary/apod";
+        Map<String, String> params = Map.of(
+                "api_key", apiKey,
+                "count", "1",
+                "hd", "true"
+        );
+        try {
+            URL endpoint = new URL(url + "?" + params.entrySet().stream()
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining("&")));
+            HttpURLConnection connection = (HttpURLConnection) endpoint.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = reader.lines().collect(Collectors.joining("\n"));
+            ctx.json(response);
+        } catch (Exception e) {
+            ctx.status(500).result("An error occurred while calling the API");
+        }
 
-//Denna api skickar dagen astronomi bild med text :)
-public void getImageAndDescriptionFromNasaApodAPI (Context ctx) {
-    String apiKey = "qUiVgPByEvA7mORI5pfuIyhmmgIcJWNIqf6JYdF0";
-    String url = "https://api.nasa.gov/planetary/apod";
-    Map<String, String> params = Map.of(
-            "api_key", apiKey,
-            "count", "1",
-            "hd", "true"
-    );
-    try {
-        URL endpoint = new URL(url + "?" + params.entrySet().stream()
-                .map(entry -> entry.getKey() + "=" + entry.getValue())
-                .collect(Collectors.joining("&")));
-        HttpURLConnection connection = (HttpURLConnection) endpoint.openConnection();
-        connection.setRequestMethod("GET");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = reader.lines().collect(Collectors.joining("\n"));
-        ctx.json(response);
-    } catch (Exception e) {
-        ctx.status(500).result("An error occurred while calling the API");
     }
-
-}
 
     //Denna metod tar fram hämtar data från endpoint utifrån id
-    public void getImageFromNasa(Context ctx) {
+    public void getImageLocation(Context ctx) {
         String id = ctx.pathParam("nasa_id");
         String url = "https://images-api.nasa.gov/metadata/" + id;
         try (InputStream inputStream = new URL(url).openStream()) {
@@ -78,9 +82,39 @@ public void getImageAndDescriptionFromNasaApodAPI (Context ctx) {
         }
     }
 
+    public void getImageFromNasa(Context ctx) {
+        String id = ctx.pathParam("nasa_id");
+        String url = "https://images-api.nasa.gov/asset/" + id;
+        try (InputStream inputStream = new URL(url).openStream()) {
+            String data = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+            ctx.json(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
-
-
+    public void searchNasa(Context ctx) {
+        String url = "https://images-api.nasa.gov/search/";
+        String apiKey = "qUiVgPByEvA7mORI5pfuIyhmmgIcJWNIqf6JYdF0";
+        Map<String, String> params = Map.of(
+                "api_key", apiKey,
+                "count", "1",
+                "hd", "true"
+        );
+        try {
+            URL endpoint = new URL(url + "?" + params.entrySet().stream()
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining("&")));
+            HttpURLConnection connection = (HttpURLConnection) endpoint.openConnection();
+            connection.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = reader.lines().collect(Collectors.joining("\n"));
+            ctx.json(response);
+        } catch (Exception e) {
+            ctx.status(500).result("An error occurred while calling the API");
+        }
+    }
 }
