5024b7b953fc0ad97e59ffb037790f6900712d7c
keyjpg <93946569+keyjpg@users.noreply.github.com>
2023-01-06 13:36:14 +0100

    !

--- Backend/src/main/java/APIRunner.java
+++ Backend/src/main/java/APIRunner.java
@@ -1,22 +1,44 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.javalin.Javalin;
+import kong.unirest.Unirest;
 
-public class APIRunner
-{
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+public class APIRunner {
 
     private Gson gson = null;
 
-    public APIRunner(){
+    public APIRunner() {
         gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         APIRunner runner = new APIRunner();
         Javalin app = Javalin.create(/*config*/)
-                .get("/", ctx -> {ctx.html("MAIN PAGE"); })
-                .get("/asset/{nasa_id}", ctx -> {ctx.html("retrieved planet image"); }) // todo:
-                .get("/id", ctx -> {ctx.html("DESCRIPTION OF IMAGE"); }) // todo:
+                .get("/", ctx -> {
+                    ctx.html("MAIN PAGE");
+                })
+                .get("/earth", ctx -> {
+
+                    //ctx.html("retrieved planet image");
+
+
+                    Map result = Unirest.get("https://images-api.nasa.gov/search")
+                            .queryString("q", "earth")
+                            .queryString("media_type", "image")
+                            .asObject(i -> new Gson().fromJson(i.getContentReader(), HashMap.class))
+                            .getBody();
+                    ctx.json(result);
+
+                }) // todo:
+                .get("/id2", ctx -> {
+                    ctx.html("DESCRIPTION OF IMAGE");
+                }) // todo:
                 .start(5000);
     }
 
---
+++ Backend/src/main/java/NasaAPI.java
@@ -0,0 +1,7 @@
+public class NasaAPI {
+
+
+    public NasaAPI() {
+
+    }
+}
